23549bde67e12f93b51e6fa5a119bceb10ec1b6a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-06 04:10:51 +0900

    Update Climbing-the-Leaderboard.cpp

--- Climbing-the-Leaderboard.cpp
+++ Climbing-the-Leaderboard.cpp
@@ -1,1 +1,35 @@
-
+vector<int> climbingLeaderboard(vector<int> ranked, vector<int> player)
+{
+    vector<int> ranks(player.size(), 1);
+    int rank = 2;
+    int n = ranked.size() - 1;
+    
+    for (int i = n; i > 0; --i)
+    {
+        if(ranked[i] < ranked[i-1])
+        {
+            rank++;
+        }
+    }
+    
+    for (size_t i = 0; i < player.size(); ++i)
+    {
+        while (player[i] >= ranked[n])
+        {
+            if(rank == 1)
+            {
+                i = player.size()-1;
+                break;
+            }
+            
+            if(ranked[n] != ranked[n-1])
+            {
+                rank--;
+            }
+            n--;
+        }
+        ranks[i] = rank;
+    }
+    
+    return ranks;
+}
